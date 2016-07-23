bc89deb077ed24af15733cc4ad8cc7634fc6854e
cybergoof <cybergoof@gmail.com>
2016-07-22 23:40:01 -0400

    initial run

---
+++ lab1run.cmd
@@ -0,0 +1,27 @@
+
+# Get the Damn Vulnerable Web App downloaded and started
+# Refence what it normally takes to install http://www.computersecuritystudent.com/SECURITY_TOOLS/DVWA/DVWAv107/lesson1/
+
+
+git clone https://github.com/nightmare-rg/dvwa-vagrant.git
+cd dvwa-vagrant
+vagrant up
+
+# here we get show that we can get to the site
+
+https://localhost:8088
+
+# Now, we need to make it so that we get the PCAP
+
+
+echo "Halting the VM"
+vagrant halt
+echo "VM has stopped"
+echo ""
+
+# Reference http://devblog.appriver.com/capture-network-packets-from-virtualbox/
+
+'c:\Program Files\Oracle\VirtualBox\VboxManage.exe' modifyvm dvwa --nictrace1 on --nictracefile1 c:\temp\dvwa.pcap
+
+# Need to start the VM through Oracle's VirtualBox
+# This is a significant problem with the network collection
